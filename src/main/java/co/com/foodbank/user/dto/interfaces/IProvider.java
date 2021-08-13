0cf781e9def6be33809633836b5744264172135b
mlondong <mauricio.londono@gmail.com>
2021-08-13 18:53:37 -0500

    _

--- src/main/java/co/com/foodbank/user/dto/interfaces/IProvider.java
+++ src/main/java/co/com/foodbank/user/dto/interfaces/IProvider.java
@@ -1,7 +1,7 @@
 package co.com.foodbank.user.dto.interfaces;
 
 import java.util.Collection;
-import co.com.foodbank.vault.interfaces.IVault;
+import co.com.foodbank.vault.dto.interfaces.IVault;
 
 /**
  * @author mauricio.londono@gmail.com co.com.foodbank.user.v1.model 15/05/2021
